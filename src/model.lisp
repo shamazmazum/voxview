d469914ff0cffa1dce7495b075b2841c40b99d0d
Vasily Postnicov <shamaz.mazum@gmail.com>
2024-12-27 15:49:14 +0300

    A bit faster model loading

--- src/model.lisp
+++ src/model.lisp
@@ -18,23 +18,23 @@
 (declaim (inline boundaryp))
 (defun boundaryp (array i j k)
   (and (not (zerop (aref array i j k)))
-       (/=
-        (loop for %i from -1 to 1 sum
-              (loop for %j from -1 to 1 sum
-                    (loop for %k from -1 to 1 sum
-                          (safe-aref array (+ i %i) (+ j %j) (+ k %k)))))
-        27)))
+       (or (zerop (aref array i j (1- k)))
+           (zerop (aref array i j (1+ k)))
+
+           (zerop (aref array i (1- j) k))
+           (zerop (aref array i (1+ j) k))
+
+           (zerop (aref array (1+ i) j k))
+           (zerop (aref array (1- i) j k)))))
 
 (sera:-> compute-boundary ((simple-array bit (* * *)))
          (values (model *) &optional))
 (defun compute-boundary (array)
   (declare (optimize (speed 3)))
   (let (list)
-    (loop for i fixnum below (array-dimension array 0) do
-          (loop for j fixnum below (array-dimension array 1) do
-                (loop for k fixnum below (array-dimension array 2) do
-                      (when (boundaryp array i j k)
-                        (push (list (float i) (float j) (float k)) list)))))
+    (do-indices (array i j k)
+      (when (boundaryp array i j k)
+        (push (list (float i) (float j) (float k)) list)))
     (make-array (list (length list) 3)
                 :element-type 'single-float
                 :initial-contents list)))
