b65c412d6e1f7960bf062f923786c698347eebbf
Vasily Postnicov <shamaz.mazum@gmail.com>
2025-01-02 10:52:09 +0300

    New model loader

--- src/model.lisp
+++ src/model.lisp
@@ -46,3 +46,49 @@
     (values
      (compute-boundary data)
      (apply #'rtg-math.vector3:make (mapcar #'float (array-dimensions data))))))
+
+
+(sera:-> connectivity ((simple-array bit (* * *))
+                       alex:non-negative-fixnum
+                       alex:non-negative-fixnum
+                       alex:non-negative-fixnum)
+         (values (or null (unsigned-byte 8)) &optional))
+(declaim (inline connectivity))
+(defun connectivity (array i j k)
+  (if (not (zerop (aref array i j k)))
+      (logior (if (zerop (safe-aref array (1- i) j k)) (ash 1 0) 0)
+              (if (zerop (safe-aref array (1+ i) j k)) (ash 1 1) 0)
+              (if (zerop (safe-aref array i (1- j) k)) (ash 1 2) 0)
+              (if (zerop (safe-aref array i (1+ j) k)) (ash 1 3) 0)
+              (if (zerop (safe-aref array i j (1- k))) (ash 1 4) 0)
+              (if (zerop (safe-aref array i j (1+ k))) (ash 1 5) 0))))
+
+(sera:defconstructor connectivity-data
+  (coord rtg-math.types:uvec3)
+  (mask  (unsigned-byte 8)))
+
+(deftype connectivity-vector () `(simple-array connectivity-data (*)))
+
+(sera:-> compute-connectivity ((simple-array bit (* * *)))
+         (values connectivity-vector &optional))
+(defun compute-connectivity (array)
+  (declare (optimize (speed 3)))
+  (let (list)
+    (do-indices (array i j k)
+      (let ((connectivity (connectivity array i j k)))
+        (when (and connectivity (not (zerop connectivity)))
+          (push (connectivity-data
+                 (rtg-math.base-vectors:v!uint i j k)
+                 connectivity)
+                list))))
+    (make-array (length list)
+                :element-type 'connectivity-data
+                :initial-contents list)))
+
+(sera:-> load-connectivity ((or string pathname))
+         (values connectivity-vector rtg-math.types:uvec3 &optional))
+(defun load-connectivity (filename)
+  (let ((data (load-data filename)))
+    (values
+     (compute-connectivity data)
+     (apply #'rtg-math.base-vectors:v!uint (array-dimensions data)))))
