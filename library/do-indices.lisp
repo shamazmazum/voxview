a360c1848cbf1d8356eb8dbc3af943a763e88ae5
Vasily Postnicov <shamaz.mazum@gmail.com>
2026-07-31 19:32:38 +0300

    Safe DO-INDICES

--- library/do-indices.lisp
+++ library/do-indices.lisp
@@ -3,10 +3,10 @@
 (defmacro do-indices ((array &rest indices) &body body)
   (let ((a (gensym)))
     `(let ((,a ,array))
-       ,(first
-         (si:foldr
+       ,(si:foldr
           (lambda (enumerated-index acc)
             (destructuring-bind (i . var)
                 enumerated-index
-              `((loop for ,var fixnum below (array-dimension ,a ,i) do ,@acc))))
-          body (si:enumerate (si:list->iterator indices)))))))
+              `(loop for ,var fixnum below (array-dimension ,a ,i) do ,acc)))
+          `(progn ,@body)
+          (si:enumerate (si:list->iterator indices))))))
